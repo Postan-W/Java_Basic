dbd7a29bf5c750978ace424565751f637d64956c
mingzhu <15216802238@163.com>
2020-11-25 18:56:48 +0800

    test增加了data5

--- Main.java
+++ Main.java
@@ -16,6 +16,7 @@ public class Main<T extends Comparable> implements GenericInterface<T>, Serializ
     private T data;
     private T data2;
     private T data4;
+    private T data5;
     //测试是否受保护
     @Override
     public T genericInterfaceData(){
