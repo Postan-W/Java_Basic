e51fcca003b740606d6f5347d0774c9d9225fb8e
mingzhu <15216802238@163.com>
2020-11-25 18:42:32 +0800

    dasd

--- Main.java
+++ Main.java
@@ -15,7 +15,6 @@ public class Main<T extends Comparable> implements GenericInterface<T>, Serializ
     private static final long serialVersionUID = 1L;
     private T data;
     private T data2;
-    private T data3;
     private T data4;
     //测试是否受保护
     @Override
